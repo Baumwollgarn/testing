1a3b4f1a71c72a3366c282482c66109312aa1d15
Baumwollgarn <92024276+Baumwollgarn@users.noreply.github.com>
2022-05-25 12:42:46 +0200

    Instances of URL + Tests

--- src/main/java/javaTesting/Resources/AppParameters.java
+++ src/main/java/javaTesting/Resources/AppParameters.java
@@ -6,13 +6,33 @@ public class AppParameters {
 
     public static final String PORT = "80";
 
+    // public static final Object BD_CON = new ClasseConnexioBBDD();
+
+    // public static Object MESSAGES_TRANSLATIONS;
+
+    // Implement SINGLETON PATTERN
+    private static AppParameters instance ;
+
+    public AppParameters() {
+
+    }
+
+    private void init() {
+        instance = new AppParameters();
+    }
+
+    public void reset() {
+        instance = null;
+    }
+    public static AppParameters getInstance() {
+        if (instance == null) {
+            instance = new AppParameters();
+        }
+        return instance;
+    }
+
     public static String getDomain() {
         return "https://" + DOMAIN + ":" + PORT;
     }
-
-    public static String getPort() {
-        return PORT;
-    }
-
 
 }
--- src/test/java/javaTesting/AppParametersTest.java
+++ src/test/java/javaTesting/AppParametersTest.java
@@ -1,6 +1,7 @@
 package javaTesting;
 
 import javaTesting.Resources.AppParameters;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,13 +10,37 @@ public class AppParametersTest {
     AppParameters appParameters = new AppParameters();
     String domain = AppParameters.DOMAIN;
     String port = AppParameters.PORT;
+    String finalDomain = AppParameters.getDomain();
 
     @Test
-    public void appParametersTest() {
-
+    public void correctURL() {
         String expectedDomain = "127.0.0.1";
-        String expectedPort = "80";
 
         assertEquals("Domain is " + expectedDomain, expectedDomain, domain);
+    }
+
+    @Test
+    public void correctPort() {
+        String expectedPort = "80";
+
+        assertEquals("Port is " + expectedPort, expectedPort, port);
+    }
+
+    @Test
+    public void correctFinalURL() {
+        String expectedFinalDomain = "https://127.0.0.1:80";
+
+        assertEquals("Final domain is " + expectedFinalDomain, expectedFinalDomain, finalDomain);
+    }
+
+    @Test
+    public void singletonCorrectness() {
+
+        AppParameters inst1 = AppParameters.getInstance();
+
+        AppParameters inst2 = AppParameters.getInstance();
+
+        Assert.assertTrue("Instances are the same", inst1 == inst2);
+
     }
 }
