0ca3cc220f4549149f10dd9a9c223686d3abfe3d
Alexander Witt <92024276+Baumwollgarn@users.noreply.github.com>
2022-05-26 10:28:52 +0200

    Created PasswordUtils

---
+++ src/main/java/javaTesting/Resources/PasswordUtils.java
@@ -0,0 +1,62 @@
+package javaTesting.Resources;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class PasswordUtils {
+
+    private static final Random RANDOM = new SecureRandom();
+    private static final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    public static String generateRandomString(int length) {
+        StringBuilder returnValue = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            returnValue.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
+        }
+        return new String(returnValue);
+    }
+
+    public static String genSecurePassword (String password, String salt) throws NoSuchAlgorithmException {
+        String returnValue = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(salt.getBytes());
+            byte[] bytes = md.digest(password.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (byte aByte : bytes) {
+                sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
+            }
+            returnValue = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return returnValue;
+    }
+
+    public static boolean verifyUserPassword(String passwordEntered, String userSalt, String userPassword) throws NoSuchAlgorithmException {
+        return userPassword.equals(genSecurePassword(passwordEntered, userSalt));
+    }
+
+
+
+    // This method can be used by anybody to check if a password
+    // is valid or not.
+    public boolean isStrongPassword(String password) {
+        // Note that we are now using regular expressions to check
+        // the password.
+        String regex = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})";
+        return password.matches(regex);
+    }
+
+    public boolean isValidUserName(String username) {
+        return (username.length() > 5);
+    }
+
+    public boolean isValidEmail(String email) {
+        String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+        return email.matches(regex);
+    }
+
+}
