5e7e574a3b2e88c29c14ec907e679dae69d88d9a
Alexander Witt <92024276+Baumwollgarn@users.noreply.github.com>
2022-06-02 10:08:11 +0200

    Collection, HashMap & HashSet tests

--- src/test/java/javaTesting/collectionsTesting/SimpleCollectionTest.java
+++ src/test/java/javaTesting/collectionsTesting/SimpleCollectionTest.java
@@ -2,11 +2,9 @@ package javaTesting.collectionsTesting;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class SimpleCollectionTest {
 
@@ -23,6 +21,114 @@ public class SimpleCollectionTest {
 
         List<String> llistNumeros = Arrays.asList(nombres);
         assertTrue(llistNumeros.size() == 10);
+    }
 
+    @Test
+    public void diesSetmana() {
+        List<String> diesSetmana = new ArrayList<>();
+        List<String> diesFeiners = new ArrayList<>();
+        List<String> diesCapDeSetmana = new ArrayList<>();
+        List<String> diesFeinersSenars = new ArrayList<>();
+        List<String> diesSenars = new ArrayList<>();
+        List<String> calculDies = new ArrayList<>();
+
+
+        initDiesFeiners(diesFeiners);
+
+        assertTrue(diesFeiners.size() == 5);
+
+        assertEquals(diesFeiners.get(0), "Dilluns");
+        assertEquals(0,diesSetmana.toArray().length);
+        assertFalse(diesSetmana.containsAll(diesFeiners));
+
+        diesSetmana.addAll(diesFeiners);
+
+        assertEquals(5,diesSetmana.toArray().length);
+        assertTrue(diesSetmana.containsAll(diesFeiners));
+
+        diesCapDeSetmana.add("Dissabte");
+        diesCapDeSetmana.add("Diumenge");
+
+        diesSetmana.addAll(diesCapDeSetmana);
+
+        assertEquals(7,diesSetmana.toArray().length);
+        assertTrue(diesSetmana.containsAll(diesCapDeSetmana));
+
+
+        diesSenars.addAll(diesSetmana);
+
+        assertTrue(diesSenars.contains("Dimarts"));
+        diesSenars.remove(1);
+        assertFalse(diesSenars.contains("Dimarts"));
+
+        diesSenars.remove("Dijous");
+        assertFalse(diesSenars.contains("Dijous"));
+
+
+        diesFeinersSenars.addAll(diesSenars);
+
+        diesFeinersSenars.removeAll(diesCapDeSetmana);
+
+        assertTrue(diesFeinersSenars.size() == 3);
+        assertFalse(diesFeinersSenars.contains("Diumenge"));
+
+        calculDies.addAll(diesSetmana);
+
+        calculDies.clear();
+
+        assertTrue(calculDies.isEmpty());
+
+        calculDies.addAll(diesSetmana);
+
+        calculDies.removeAll(diesCapDeSetmana);
+
+        assertEquals(5,calculDies.size());
+
+    }
+
+    @Test
+    public void provesAmbMapsTest() {
+        Map<String,String> mapa = new java.util.HashMap<>();
+        mapa.put("fons","vermell");
+        mapa.put("menu","verd");
+        mapa.put("taula","blau");
+
+        assertEquals(3, mapa.size());
+        assertTrue(mapa.containsKey("fons"));
+        assertTrue(mapa.containsValue("vermell"));
+
+        mapa.remove("fons");
+
+        assertEquals(2, mapa.size());
+
+        mapa.remove("ClauInexistent");
+
+        assertEquals(2, mapa.size());
+
+        assertEquals(mapa.get("menu"), "verd");
+
+
+    }
+
+    public void initDiesFeiners(List<String> dies) {
+        dies.add("Dilluns");
+        dies.add("Dimarts");
+        dies.add("Dimecres");
+        dies.add("Dijous");
+        dies.add("Divendres");
+    }
+
+    @Test
+    public void provesSetTest() {
+
+        Set<String> dies = new HashSet<>();
+
+        dies.add("Dilluns");
+        dies.add("Dimarts");
+        dies.add("Dimecres");
+        dies.add("Dijous");
+        dies.add("Divendres");
+
+        assertEquals(5,dies.size());
     }
 }
